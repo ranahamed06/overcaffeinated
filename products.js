f9c64b78783d765b4536273156967419a55231e4
ranahamed06 <rana_alaa@aucegypt.edu>
2025-08-14 12:14:43 +0300

    Update products.js

--- products.js
+++ products.js
@@ -2,60 +2,96 @@ const products = [{
     id: 1,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 },{
     id: 2,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 3,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 4,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 5,
     name: 'Product-name',
     price: '$10' ,
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 6,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.` 
 }, {
     id: 7,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.` 
 }, {
     id: 8,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.` 
 }, {
     id: 9,
     name: 'Product-name',
     price: '$10' ,
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 10,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 },{
     id: 11,
     name: 'Product-name',
     price: '$10' ,
-    image: 'pic-16.jpg'
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }, {
     id: 12,
     name: 'Product-name',
     price: '$10',
-    image: 'pic-16.jpg' 
+    image: 'pic-16.jpg',
+    info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non aliquam ipsum, 
+    facilisis molestie ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+    Quisque in tellus placerat lacus malesuada interdum sit amet ut leo.`
 }];
